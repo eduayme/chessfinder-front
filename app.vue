6aca2343e9c0f9798004e7f699a4bd562895b4de
Eduard Aymerich <eduardaymerich@gmail.com>
2023-06-22 15:55:02 +0200

    feature: add capture clicks

--- app.vue
+++ app.vue
@@ -169,6 +169,10 @@
               class="md:invisible md:group-hover:visible"
               :to="website"
               target="_blank"
+              @click="posthog.capture('click-website', {
+                name: name,
+                website: website
+              });"
             >
               <UIcon name="i-heroicons-link" class="text-lg" />
               {{ $t("website") }}
@@ -179,6 +183,10 @@
               class="md:invisible md:group-hover:visible"
               :to="ranking"
               target="_blank"
+              @click="posthog.capture('click-ranking', {
+                name: name,
+                ranking: ranking
+              });"
             >
               <UIcon name="i-heroicons-user-group" class="text-lg" />
               {{ $t("ranking") }}
@@ -189,6 +197,10 @@
               class="md:invisible md:group-hover:visible"
               :to="info"
               target="_blank"
+              @click="posthog.capture('click-info', {
+                name: name,
+                info: info
+              });"
             >
               <UIcon name="i-heroicons-information-circle" class="text-lg" />
               {{ $t("info") }}
@@ -206,13 +218,6 @@
 </template>
 
 <script setup>
-onMounted(() => {
-  if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
-    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-    posthog.init('phc_u5d313g4BNHKKC0cxsWNepdD24UVLH7Nj6UzCWYqoeS',{api_host:'https://eu.posthog.com'})
-  }
-})
-
 const runtimeConfig = useRuntimeConfig()
 const page = ref(1)
 const search = ref("")
@@ -221,6 +226,13 @@ const notStarted = ref(false)
 const minDate = ref(null)
 const filterControl = ref("")
 const filterFederation = ref("")
+
+onMounted(() => {
+  if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
+    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('phc_u5d313g4BNHKKC0cxsWNepdD24UVLH7Nj6UzCWYqoeS',{api_host:'https://eu.posthog.com'})
+  }
+})
 
 const getIcon = (name) => {
   if (name === "standard") {
@@ -237,9 +249,28 @@ watch(page, () => {
   refresh()
 })
 
-watch([search, notStarted, filterControl, filterFederation], () => {
+watch(search, (newValue) => {
   refresh()
   page.value = 1
+  posthog.capture('filter-search', {
+    search: newValue
+  });
+})
+
+watch(filterControl, (newValue) => {
+  refresh()
+  page.value = 1
+  posthog.capture('filter-control', {
+    timeControl: newValue
+  });
+})
+
+watch(filterFederation, (newValue) => {
+  refresh()
+  page.value = 1
+  posthog.capture('filter-federation', {
+    federation: newValue
+  });
 })
 
 watch(notStarted, (newValue) => {
@@ -250,6 +281,10 @@ watch(notStarted, (newValue) => {
     minDate.value = null
   }
   refresh()
+  page.value = 1
+  posthog.capture('filter-notStarted', {
+    minDate: minDate.value
+  });
 })
 
 const formatDate = (date) => {
